547ed97af68a613d21a68c26d1a75b2f1a174d88
luckmer <woszczo04@gmail.com>
2023-07-29 21:57:44 +0200

    added new packages

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import Bot from "./src/Bot";
 
-new Bot(
+export const bot = new Bot(
   new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages]
   })
