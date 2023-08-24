b3a0328eaa27247e9ee85260839c9099c01b7fdd
luckmer <woszczo04@gmail.com>
2023-08-24 21:20:13 +0200

    added music queue with embed maker class

---
+++ src/utils/EmbedMaker.ts
@@ -0,0 +1,26 @@
+import { EmbedBuilder } from "discord.js";
+import { InfoData } from "play-dl";
+
+export class EmbedMaker {
+  getSongModal(song: InfoData) {
+    const icons = song.video_details.channel?.icons;
+
+    const chanelIcon = icons?.length ? icons[0].url : undefined;
+
+    return new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle(song.video_details.title ?? "--")
+      .setURL(song.video_details.channel?.url ?? null)
+      .setAuthor({
+        name: song.video_details.channel?.name ?? "--",
+        iconURL: chanelIcon,
+        url: song.video_details.channel?.url
+      })
+      .setThumbnail(typeof chanelIcon === "undefined" ? null : chanelIcon)
+      .setTimestamp();
+  }
+
+  getContentModal(content: string) {
+    return new EmbedBuilder().setColor(0x0099ff).setTitle(content);
+  }
+}
