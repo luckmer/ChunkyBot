71c40c35a193bef1653eb4b3e848fb61162dd5b5
luckmer <woszczo04@gmail.com>
2023-10-07 16:09:12 +0200

    added todo

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -8,6 +8,7 @@ module.exports = {
     const guildID = interaction.guild!.id;
     const queue = bot.queues.get(guildID);
     const songs: AudioMaker[] = Array.from(queue?.queues.values() ?? []).flat();
+    //TODO: display songs
     console.log(songs);
     await interaction.reply("queue!");
   }
