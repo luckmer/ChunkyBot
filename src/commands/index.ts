e01e8a8ca743b287d863941f951d66c4086db883
luckmer <woszczo04@gmail.com>
2023-12-02 12:52:25 +0100

    connected new commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,8 @@
-export * as play from "./music/play";
+export * as help from "./music/help";
 export * as list from "./music/list";
-export * as search from "./music/search";
 export * as pause from "./music/pause";
+export * as ping from "./music/ping";
+export * as play from "./music/play";
 export * as resume from "./music/resume";
+export * as search from "./music/search";
 export * as volume from "./music/volume";
--- src/commands/music/list.ts
+++ src/commands/music/list.ts
@@ -1,53 +1,38 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import { bot } from "../../../index";
-import { AudioMaker, EmbedMaker } from "../../utils";
+import { EmbedMaker } from "../../utils";
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("list").setDescription("list of songs"),
-  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+  data: new SlashCommandBuilder()
+    .setName("list")
+    .setDescription("Allows you to view the list of songs in the playlist."),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const embedMaker = new EmbedMaker();
     await interaction.deferReply({ ephemeral: true });
-    const embed = new EmbedMaker();
     const guildID = interaction.guild!.id;
     const queue = bot.queues.get(guildID);
-    const songs: AudioMaker[] = Array.from(queue?.queues.values() ?? []).flat();
 
-    // Create an array to store author information for all songs
-    const authorsInfo = songs.map((song: AudioMaker) => ({
-      name: song.songInfo.video_details.channel?.name ?? "Unknown Author",
-      iconURL: song.songInfo.video_details.channel?.icons?.[0] ?? undefined,
-      url: song.songInfo.video_details.url
-    }));
+    let helpEmbed = embedMaker.getValueContentModal(interaction.client.user!.username);
 
-    // let playlistEmbed = new EmbedBuilder()
-    //   .setTitle("List of Songs")
-    //   .setDescription(songs.map((song: AudioMaker, index: number) => `${index + 1}. ${song.song.title}`).join("\n\n"))
-    //   .setURL(songs.map((song: AudioMaker) => song.songInfo.video_details.url).join("\n\n"))
-    //   .setColor("#F8AA2A");
+    if (!queue?.songs.length) {
+      await interaction.editReply({
+        embeds: [embedMaker.getContentModal("Sorry, but list of songs in the playlist is empty.", "#F8AA2A")]
+      });
+      return;
+    }
 
-    // // Set the author field to display information about all authors
-    // playlistEmbed.setAuthor({
-    //   name: "Authors Information"
-    // });
-
-    // // Add author information for each song in a field
-    // playlistEmbed.addFields({
-    //   name: "Authors",
-    //   value: authorsInfo
-    //     .map(
-    //       (author, index) =>
-    //         `[${index + 1}. ${author.name}](${author.url}) ${
-    //           author.iconURL ? `[![Icon](${author.iconURL})](${author.url})` : ""
-    //         }`
-    //     )
-    //     .join("\n")
-    // });
-
-    const embeds = songs.map((song) => embed.getSongModal(song.songInfo));
-
-    await interaction.channel?.send({
-      content: "List of Songs",
-      embeds: embeds
+    (queue?.songs ?? []).forEach((song, index) => {
+      helpEmbed.addFields({
+        name: `**${index + 1}: ${song.song.title}**`,
+        value: `duration: ${parseInt(song.song.duration.toString()) / 60}`,
+        inline: false
+      });
     });
-    interaction.deleteReply().catch(console.error);
+
+    helpEmbed.setTimestamp();
+
+    return interaction.editReply({ embeds: [helpEmbed] }).catch(() => {
+      interaction.deleteReply().catch(console.error);
+    });
   }
 };
